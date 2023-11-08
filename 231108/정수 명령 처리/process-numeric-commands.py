35b67b40f2af560149feb99a4eca58189e028768
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-11-08 21:45:49 +0900

    Title: 정수 명령 처리, Time: 276ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 231108/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,41 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def empty(self):
+        if not self.size():
+            return 1
+        return 0
+
+    def size(self):
+        return len(self.items)
+
+    def pop(self):
+        if self.empty():
+            raise Exception("Stack is empty")
+        return self.items.pop()
+
+    def top(self):
+        if self.empty():
+            raise Exception("Stack is empty")
+        return self.items[-1]
+
+
+s = Stack()
+cmd_num = int(input())
+for _ in range(cmd_num):
+    cmd = list(input().split())
+    if cmd[0] == "push":
+        s.push(cmd[1])
+    else:
+        if cmd[0] == "size":
+            print(s.size())
+        elif cmd[0] == "empty":
+            print(s.empty())
+        elif cmd[0] == "pop":
+            print(s.pop())
+        elif cmd[0] == "top":
+            print(s.top())
