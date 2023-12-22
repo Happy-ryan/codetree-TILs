361ad0c0b93a96c23c0099cd455f6b4779aef077
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2023-12-22 14:28:41 +0900

    Title: 가장 짧은 부분합, Time: 150ms, Memory: 32MB, Status: Passed - CodeTree

--- 231222/가장 짧은 부분합/shortest-subtotal.py
+++ 231222/가장 짧은 부분합/shortest-subtotal.py
@@ -2,20 +2,19 @@ n, s = map(int, input().split())
 numbers = list(map(int, input().split()))
 
 res = 100001
-sum_val =0
+sum_val = numbers[0]
 r = 0
-flag = False
+
 for l in range(n):
-    while r < n and sum_val + numbers[r] < s:
-        sum_val += numbers[r]
+    while r + 1 < n and sum_val < s:
+        sum_val += numbers[r + 1]
         r += 1
+
     if sum_val >= s:
-        flag = True
-    res = min(res, r - l + 1)
+        res = min(res, r - l + 1)
     sum_val -= numbers[l]
 
-    
-if flag:
-    print(res)
-else:
+if res == 100001:
     print(-1)
+else:
+    print(res)
