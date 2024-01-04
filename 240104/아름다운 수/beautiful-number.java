e075cbd41f2f273f36800fa5f672463a0241b52d
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-04 18:23:49 +0900

    Title: 아름다운 수, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240104/아름다운 수/beautiful-number.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static List<Integer> ans = new ArrayList<>();
+    public static int cnt = 0;
+    public static int n;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+
+        dfs(0);
+        System.out.println(cnt);
+        
+    }
+    public static boolean isBeautiful(){
+        for(int start = 0; start < n; start += ans.get(start)){
+            if(start + ans.get(start) - 1 >= n){
+                return false;
+            }
+            for(int end = start; end < start; end++){
+                if(ans.get(end) != ans.get(start)){
+                    return false;
+                }
+            } 
+        }
+        return true;
+    }
+    public static void dfs(int level){
+        if(level == n){
+            if(isBeautiful()){
+                cnt++;
+            }
+            return;
+        }
+        for(int i = 1; i < 5; i++){
+            ans.add(i);
+            dfs(level + 1);
+            ans.remove(ans.size() - 1);
+        }
+    }
+}
