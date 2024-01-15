4e3d7eacbe2df90baaba3477f971ac6db8e8cea8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-15 17:46:40 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 199ms, Memory: 26MB, Status: Passed - CodeTree

--- 240115/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240115/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -4,11 +4,14 @@ from collections import Counter
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
+# 초기pos는 1base고 new_pos 0base > 둘이 맞춰야함.
+# move할 때 초기 pos를 0base로 바꾸기 위해서 r - 1, c - 1을 하는데 그러면 그 이후 new_pos부터는 0base임에도 -1을 하게 됨.
 def change_0base(pos):
     new_pos = []
     for r, c in pos:
         new_pos.append([r - 1, c - 1])
     return new_pos
+
 
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < n
@@ -19,12 +22,11 @@ def move(r, c):
     final_val = 0
     for k in range(4):
         nr, nc = r + dr[k], c + dc[k]
-        if in_range(nr, nc):
+        if in_range(nr, nc) and board[nr][nc] > final_val :
             # 상하좌우로 미리 dr,dc를 설정함
             # 따라서 같은 값이 4개 나오더라도 >= 하지 않아서 우선순위대로 움직이게 된다!
-            if board[nr][nc] > final_val:
-                final_val = board[nr][nc]
-                final_dir = (nr, nc)
+            final_val = board[nr][nc]
+            final_dir = (nr, nc)
     return final_dir
 
 
@@ -35,7 +37,8 @@ def one_time_move_all():
     for r, c in pos:
         final_dir = move(r, c)
         new_pos_candidate.append(final_dir)
-
+    
+    # 중복되는 곳 지우기
     dic = Counter(new_pos_candidate)
     new_pos = [key for key, count in dic.items() if count == 1]
     pos = new_pos
