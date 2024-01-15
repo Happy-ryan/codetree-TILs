5f67da48602461b67a7d1b11895ce721dc94e4ab
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-15 16:07:07 +0900

    Title: 알파벳과 사칙연산, Time: 334ms, Memory: 27MB, Status: Passed - CodeTree

--- 240115/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 240115/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def find_cmd_and_number_count(s: str):
     cmd_list, number_list = [], []
     for x in s:
@@ -5,31 +7,46 @@ def find_cmd_and_number_count(s: str):
             cmd_list.append(x)
         else:
             number_list.append(x)
+
     return cmd_list, number_list
 
 
-def calculate(cmd_list, number_list):
-    tmp = number_list[0]
+def calculate(cmd_list, number_list, number_dic):
+    tmp = number_dic[number_list[0]]
     for cmd, number in zip(cmd_list, number_list[1:]):
         if cmd == "+":
-            tmp += number
+            tmp += number_dic[number]
         elif cmd == "-":
-            tmp -= number
+            tmp -= number_dic[number]
         elif cmd == "*":
-            tmp *= number
+            tmp *= number_dic[number]
     return tmp
 
 
+def assign_alphabet_number(al_list, ans):
+    number_dic = defaultdict()
+    al_list = list(set(al_list))
+    al_list.sort()
+    for al, num in zip(al_list, ans):
+        number_dic[al] = num
+
+    return number_dic
+
+
+
+max_val = -2**(31)
+
 def solution(s: str):
-    cmd_list, number_list = find_cmd_and_number_count(s)
+    cmd_list, al_list = find_cmd_and_number_count(s)
+
+    k = len(set(al_list))
 
     ans = []
-    max_val = -2**(31)
     def dfs(level):
-        #nolocal이 뭐야?
-        nonlocal max_val
-        if level == len(number_list):
-            max_val = max(max_val, calculate(cmd_list, ans))
+        global max_val
+        if level == k:
+            number_dic = assign_alphabet_number(al_list, ans)
+            max_val = max(max_val, calculate(cmd_list, al_list, number_dic))
             return
         for i in range(1, 5):
             ans.append(i)
@@ -42,3 +59,5 @@ def solution(s: str):
 
 s = input()
 solution(s)
+
+# 틀린이유1: 같은 알파벳에 대해서는 같은 값이 들어가야한다!
