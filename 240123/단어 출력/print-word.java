c9f2df71031ee07186b217b106ec0688f5fde6d9
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-23 13:23:27 +0900

    Title: 단어 출력, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240123/단어 출력/print-word.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int MAX = 50;
+    private static String[][] board = new String[MAX][MAX];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+    }
+}
