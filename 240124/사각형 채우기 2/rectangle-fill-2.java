53c4cabc106725b624a98b73dd2e56e0263f43e8
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-01-24 20:07:49 +0900

    Title: 사각형 채우기 2, Time: 194ms, Memory: 10MB, Status: Passed - CodeTree

--- 240124/사각형 채우기 2/rectangle-fill-2.java
+++ 240124/사각형 채우기 2/rectangle-fill-2.java
@@ -8,19 +8,11 @@ public class Main {
         int n = sc.nextInt();
 
         int[] dp = new int[n + 1];
-
-        if (n == 1){
-            System.out.println(1);
-        }else if(n == 2){
-            System.out.println(3);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD;
         }
-        else{
-            dp[1] = 1;
-            dp[2] = 3;
-            for(int i = 3; i <= n; i++){
-                dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD;
-            }
-            System.out.println(dp[n]);
+        System.out.println(dp[n]);
         }
-    }
 }
