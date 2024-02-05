87e05023b5adc5ebc98ea25cae496c97e51cc1c2
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-05 20:00:54 +0900

    Title: 마을 구분하기, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240205/마을 구분하기/seperate-village.py
+++ 240205/마을 구분하기/seperate-village.py
@@ -2,6 +2,7 @@ n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False for _ in range(n)] for _ in range(n)]
 
+
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < n and not visited[r][c] and board[r][c] == 1
 
@@ -25,10 +26,11 @@ for r in range(n):
     for c in range(n):
         if board[r][c] == 0 or visited[r][c] == True:
             continue
-        cnt = 0
+        visited[r][c] = True
+        cnt = 1
         dfs(r, c)
-        if cnt != 0:
-            cnts.append(cnt)
+        cnts.append(cnt)
+
 
 cnts.sort()
 print(len(cnts))
