a49ec1e8ddaa1b0b01e81dde444cf53e40a5ad68
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-06 20:37:03 +0900

    Title: 자연수 n개의 합, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240206/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ 240206/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -3,20 +3,19 @@ s = int(input())
 def sum_val(n):
     return (n * (n + 1)) // 2
 
-
 # s 보다 작은 1 ~ n까지의 합에서 n의 최댓값
 # sum을 그대로 하면 시간초과가 100퍼 난다!
 # 다른 방법이 필요해보인다.
 def binary_search(target: int):
-    l, r = 0, int(1e10)
+    l, r = 1, int(1e18)
     ans = -1
     while l <= r:
         mid = (l + r) // 2
-        if sum_val(mid) <= target:
+        if sum_val(mid) < target:
             ans = mid
-            l += 1
+            l = mid + 1
         else:
-            r -= 1
+            r = mid - 1
 
     return ans
 
