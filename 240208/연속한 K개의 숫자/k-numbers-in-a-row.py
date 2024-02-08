b104cf34dc9ff5d4a32aa0a85f7879b4450a1ab4
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-08 14:38:07 +0900

    Title: 연속한 K개의 숫자, Time: 156ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240208/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 240208/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,4 +1,5 @@
 # k개의 숫자라는 size가 고정..슬라이딩윈도우도 쌉가능할듯..?
+# 존재하는 애들보다 사라진 애들에 초점을 맞추는게 필요함!
 n, k, b = map(int, input().split())
 exclude = [int(input()) for _ in range(b)]
 
@@ -25,4 +26,22 @@ def sliding_window(n, k, b, exclude):
     
     return ans
 
-print(sliding_window(n,k,b,exclude))
+
+def prefix_sum(n, k, b, exclude):
+    nums = list(range(1, n + 1))
+    exclude_list = [0] * (n + 1)
+    for x in exclude:
+        exclude_list[x] = 1
+    psum = [0] * (n + 2)
+    for i in range(n + 1):
+        psum[i + 1] = exclude_list[i] + psum[i]
+    
+    ans = k
+    for i in range(n + 1 - k + 1):
+        ans = min(ans, psum[i + k] - psum[i])
+
+    return ans
+
+
+# print(sliding_window(n, k, b, exclude))
+print(prefix_sum(n, k, b, exclude))
