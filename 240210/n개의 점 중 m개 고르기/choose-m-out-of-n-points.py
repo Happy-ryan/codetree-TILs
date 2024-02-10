c4ab464b34e67e29ca2c5d98cd354c9dd2afb68e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-10 14:22:09 +0900

    Title: n개의 점 중 m개 고르기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -0,0 +1,43 @@
+from itertools import combinations
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def dist(x1, y1, x2, y2):
+    return (x2 - x1) ** 2 + (y2 - y1) ** 2
+
+# 두 점 사이의 거리는 완탐을 돌리자 > 최대 20C10 = 184756 > 시간초과 걸리겠는데..?
+# nCm * mC2 = 20C10 * 10C2 
+def cal(ans):
+    min_val = 1000
+    for p1, p2 in combinations(ans, 2):
+        min_val = min(min_val, dist(p1.x, p1.y, p2.x, p2.y))
+    return min_val
+
+
+n, m = map(int, input().split())
+points = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.append(Point(x, y))
+
+ans = []
+final_ans = 10000
+def combination(cur_idx, cnt):
+    global final_ans
+    if cur_idx == n:
+        if cnt == m:
+            final_ans = min(final_ans, cal(ans))
+        return
+
+    ans.append(points[cur_idx])
+    combination(cur_idx + 1, cnt + 1)
+    ans.pop()
+
+    combination(cur_idx + 1, cnt)
+
+combination(0, 0)
+print(final_ans)
