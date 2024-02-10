3ed48707a2ec7f8acf00b05ef06bc358d9d7b1ac
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-10 17:17:18 +0900

    Title: 떨어지는 1자 블록, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240210/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240210/떨어지는 1자 블록/falling-horizontal-block.py
@@ -5,12 +5,15 @@ board = [list(map(int, input().split())) for _ in range(n)]
 
 row = 0
 while True:
-    # row가 지속적으로 초기화됨. while문 밖으로 이동
     can_move = True
-    for col in range(k, m + k):
-        if board[row][col] == 1:
-            can_move = False
-            break
+    # row가 지속적으로 초기화됨. while문 밖으로 이동
+    if row >= n:
+        can_move = False
+    else:
+        for col in range(k, m + k):
+            if board[row][col] == 1:
+                can_move = False
+                break
 
     if not can_move:
         for col in range(k, m + k):
