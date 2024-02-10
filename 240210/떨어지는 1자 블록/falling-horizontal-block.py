472c0fedb05aa58b51a7d38fb625e82f041c38d5
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-10 17:10:35 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240210/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,23 @@
+n, m, k = map(int, input().split())
+# 블록의 시작점(가장 왼쪽)
+k -= 1
+board = [list(map(int, input().split())) for _ in range(n)]
+
+row = 0
+while True:
+    # row가 지속적으로 초기화됨. while문 밖으로 이동
+    can_move = True
+    for col in range(k, m + k):
+        if board[row][col] == 1:
+            can_move = False
+            break
+
+    if not can_move:
+        for col in range(k, m + k):
+            board[row - 1][col] = 1
+        break
+
+    row += 1
+
+for rowrow in board:
+    print(*rowrow)
