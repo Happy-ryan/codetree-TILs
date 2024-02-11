78fd9202f618d4e082aa222ddd54455197daf4b3
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-11 13:53:08 +0900

    Title: 상한 귤, Time: 253ms, Memory: 27MB, Status: Passed - CodeTree

--- 240211/상한 귤/oranges-have-gone-bad.py
+++ 240211/상한 귤/oranges-have-gone-bad.py
@@ -13,7 +13,7 @@ dc = [0, 0, -1, 1]
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < n
 
-def bfs(r, c):
+def bfs_(r, c):
     dq = deque([])
     in_queue = [[False for _ in range(n)] for _ in range(n)]
     visited = [[inf for _ in range(n)] for _ in range(n)]
@@ -41,15 +41,46 @@ def bfs(r, c):
     return min_val
 
 
-for r in range(n):
-    for c in range(n):
-        if board[r][c] == 0:
-            print(-1, end= " ")
-        elif board[r][c] == 1:
-            ans = bfs(r, c)
-            if ans >= inf:
-                ans = -2
-            print(ans, end=" ")
-        else:
-            print(0, end= " ")
-    print()
+def find(board):
+    starts = []
+    for r in range(n):
+        for c in range(n):
+            if board[r][c] == 2:
+                starts.append((r, c))
+    return starts
+
+def bfs(starts):
+    dq = deque([])
+    in_queue = [[False for _ in range(n)] for _ in range(n)]
+    visited = [[0 for _ in range(n)] for _ in range(n)]
+
+    for r, c in starts:
+        dq.append((r, c))
+        in_queue[r][c] = True
+
+    while dq:
+        cr, cc = dq.popleft()
+        for dir in range(4):
+            nr = cr + dr[dir]
+            nc = cc + dc[dir]
+            if in_range(nr, nc) and not in_queue[nr][nc] and board[nr][nc] == 1:
+                dq.append((nr, nc))
+                board[nr][nc] = 2 # 귤이 상함 1 > 2
+                in_queue[nr][nc] = True
+                visited[nr][nc] = visited[cr][cc] + 1
+
+    
+    for r in range(n):
+        for c in range(n):
+            if board[r][c] == 0:
+                visited[r][c] = -1
+            # 귤이 있는데(1) & 방문하지 않음(상하지않음)
+            elif board[r][c] == 1 and visited[r][c] == 0:
+                visited[r][c] = -2
+
+
+    for row in visited:
+        print(*row)
+
+starts = find(board)
+bfs(starts)
