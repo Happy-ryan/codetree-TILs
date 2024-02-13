61d6bb003b61cc4c50131d8547cd8cb7e5d738ce
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-13 17:46:58 +0900

    Title: 수 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240213/수 채우기/fill-in-number.py
@@ -0,0 +1,34 @@
+# 동전 문제 중 동전끼리 배수가 아닌 경우의 그리디!
+# 그리디, dp도 가능해보임
+# 동전 자체를 중복사용가능하기때문에 dp 쉬운 유형
+
+inf = int(1e9)
+
+money = int(input())
+dp = [-1 for _ in range(money + 1)]
+
+def dpf(money):
+    if money == 0:
+        return 0
+
+    ret = inf
+    is_start = True
+    for x in [2, 5]:
+        if x <= money:
+            ret = min(ret, dpf(money - x) + 1)
+            is_start = False
+    # 1원..
+    # inf 불가능!
+    if is_start:
+        return inf
+
+    dp[money] = ret
+
+    return ret
+
+
+ans = dpf(money)
+if ans >= inf:
+    ans = -1
+
+print(ans)
