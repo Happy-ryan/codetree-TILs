3bb398fbeb33ae4c84cd38b1be479908783bc98e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-13 18:09:38 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 155ms, Memory: 32MB, Status: Wrong Answer - CodeTree

---
+++ 240213/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -0,0 +1,17 @@
+# 정렬은 불가
+n = int(input())
+nums = list(map(int, input().split()))
+
+ans = 0
+l, r = 0, 0
+while r < n:
+    sum_val = nums[r] - nums[l]
+    if ans <= sum_val:
+        ans = sum_val
+    else:
+        sum_val = 0
+        l = r
+    r += 1
+
+
+print(ans)
