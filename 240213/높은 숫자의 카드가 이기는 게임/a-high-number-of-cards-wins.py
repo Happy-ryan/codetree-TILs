748cd674fe03a5ac1da7d9f04ebe741a4d3db661
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-13 22:26:20 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 240ms, Memory: 26MB, Status: Passed - CodeTree

--- 240213/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240213/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -7,12 +7,17 @@ memo = [0 for _ in range(n)]
 for i in range(n):
     memo[i] = 2 * n - card_b[i] - (n - i - 1)
 
-print(n - memo.count(0))
-
-# n^2 이므로 완탐 불가!!
-# 1 4 6
-# 2 3 5 > 2, 5 > 2제출
-
-# 1  5 - 2 = 3
-# 4  2 - 1 = 1
-# 6  0 = 0
+# 상대편의 가장 큰 값부터 제거
+# 우리의 가장 큰 카드도 죽기 때문에 중간에 반드시 지는 경우가 존재함!
+# 사용한 카드수를 기록해야한다.
+# 나보다 큰 값 - 뽑을 수 있는 카드의 수(memo[val])> 사용한 카드의 수(used)
+used_card_num = 0
+score = 0
+for val in range(n - 1, -1, -1):
+    if memo[val] > 0 and memo[val] > used_card_num:
+        score += 1
+        used_card_num += 1
+    #     print(f"나의 위치: {memo[val]}, 사용한 카드 수: {used_card_num}")
+    # else:
+    #     print(f"내가 쓸 수 있는 카드 수: {memo[val]} < 사용한 카드 수: {used_card_num}")
+print(score)
