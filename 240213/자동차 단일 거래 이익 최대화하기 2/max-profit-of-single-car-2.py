d2c942231ed1e03810ba5b6504448b9659a74842
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-13 19:49:13 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 174ms, Memory: 31MB, Status: Passed - CodeTree

--- 240213/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240213/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -1,16 +1,19 @@
 # 정렬은 불가
+# 쌍 문제는 하나을 고정한다!!
 n = int(input())
 nums = list(map(int, input().split()))
+# 누적 최소값
+inf = int(1e9)
+pmin = [inf for _ in range(n + 1)]
+
+ans = inf
+for i in range(n):
+    pmin[i + 1] = min(pmin[i], nums[i])
 
 ans = 0
-l, r = 0, 0
-while r < n:
-    sum_val = nums[r] - nums[l]
-    if ans <= sum_val:
-        ans = sum_val
-    else:
-        l = r
-    r += 1
-
+for i in range(n):
+    if nums[i] == pmin[i + 1]:
+        continue
+    ans = max(ans, nums[i] -pmin[i + 1])
 
 print(ans)
