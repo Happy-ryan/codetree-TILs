63ab84e0951a7f160e1d68af2648e282c5d4f9d4
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-14 15:50:03 +0900

    Title: 부분 수열의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240214/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 240214/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -1,34 +1,55 @@
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 dp = [[-1 for _ in range(m + 1)] for _ in range(n + 1)]
-inf = int(1e9)
-# 풀이1
-# 특정한 값을 만드는 dp유형, 순서 고정, 1개만 사용!! > 거꾸로 봐야해!!
-# dpf(i, j) = i번째 숫자를 썼을 때 합이 J이다.
-def dpf(i, j):
-    if i == 0:
-        if j == 0:
-            return 0
-        else:
-            return inf
-    if j < 0:
-        return inf
 
-    if dp[i][j] != -1:
-        return dp[i][j]
+# # 풀이1
+# # 특정한 값을 만드는 dp유형, 순서 고정, 1개만 사용!! > 거꾸로 봐야해!!
+# # dpf(i, j) = i번째 숫자를 썼을 때 합이 J이다.
+# def dpf(i, j):
+#     if i == 0:
+#         if j == 0:
+#             return 0
+#         else:
+#             return inf
+#     if j < 0:
+#         return inf
 
-    ret = min(dpf(i - 1, j), dpf(i - 1, j - nums[i]) + 1)
+#     if dp[i][j] != -1:
+#         return dp[i][j]
 
-    dp[i][j] = ret
+#     ret = min(dpf(i - 1, j), dpf(i - 1, j - nums[i]) + 1)
 
-    return ret
+#     dp[i][j] = ret
 
-ans = dpf(n - 1, m)
-if ans >= inf:
-    ans = 'No'
-else:
-    ans = 'Yes'
+#     return ret
 
-print(ans)
+# ans = dpf(n - 1, m)
+# if ans >= inf:
+#     ans = 'No'
+# else:
+#     ans = 'Yes'
 
-#
+# print(ans)
+
+def solution_1():
+    global nums
+    # dp[i][j] i번째 동전을 사용했을 때 금액j를 만드는 최소 동전의 수!!
+    # 중복 사용이 불가   
+    inf = int(1e9) 
+    nums = [0] + nums
+    dp = [[inf for _ in range(m + 1)] for _ in range(n + 1)]
+    dp[0][0] = 0
+    inf = int(1e9)
+    for i in range(1, n + 1):
+        for j in range(m, -1, -1):
+            dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - nums[i]] + 1)
+
+    ans = dp[n][m]
+    if ans >= inf:
+        ans = 'No'
+    else:
+        ans = 'Yes'
+    
+    return ans
+
+print(solution_1())
