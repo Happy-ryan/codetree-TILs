a8686c342e4ccbcaf9e7ec82f15f1b4db3fa9066
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-14 13:01:07 +0900

    Title: 1, 2, 5 더하기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240214/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,26 @@
+# 중복을 사용할 수 있는 dp
+n = int(input())
+# n원을 나타낼 수 있는 경우의 수
+
+# dpf(i) = dpf(i-1) + dfs(i-2) + dpf(i-5)
+
+#풀이1
+def solution_1(n):
+    dp = [-1 for _ in range(n + 1)]
+    # dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 5]
+    # 특정값을 만드는 방법의 수 + 순서가 다르면 다른 조합!
+    mod = 10007
+    # 5 - 5 = 0 1개 필요..dp[0] = 1
+    dp[0] = 1
+
+    for i in range(1, n + 1):
+        if i == 1:
+            dp[i] = (dp[i - 1]) % mod
+        elif i < 5:
+            dp[i] = (dp[i - 1] + dp[i - 2]) % mod
+        else:
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 5]) % mod
+
+    return dp[n]
+
+print(solution_1(n))
