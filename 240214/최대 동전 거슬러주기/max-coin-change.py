af5f718da0ba6ef5d5bcf60d96db91d28bae809e
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-14 13:34:54 +0900

    Title: 최대 동전 거슬러주기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240214/최대 동전 거슬러주기/max-coin-change.py
+++ 240214/최대 동전 거슬러주기/max-coin-change.py
@@ -19,4 +19,28 @@ def solution_1(m):
         ans = -1
     return ans
 
-print(solution_1(m))
+def solution_2(m):
+    inf = int(1e9)
+    dp = [-inf for _ in range(m + 1)]
+
+    def dpf(money):
+        if dp[money] != -1:
+            return dp[money]
+        # 동전의 수이므로!!
+        if money == 0:
+            return 0
+        ret = 0
+        for coin in coins:
+            if coin <= money:
+                ret = max(ret, dpf(money - coin) + 1)
+        
+        dp[money] = ret
+
+        return ret
+
+    ans = dpf(m)
+    if ans < 0:
+        ans = -1
+    return ans
+
+print(solution_2(m))
