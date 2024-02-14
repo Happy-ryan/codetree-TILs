363346dbcc095eb54eb6743196e27d70fe20da6b
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-14 22:38:48 +0900

    Title: 배낭 채우기 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240214/배낭 채우기 2/knapsack-2.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+infos = [list(map(int, input().split())) for _ in range(n)]
+
+def sol_1():
+    inf = int(1e9)
+    # dp[i] = i무게를 달성했을 때의 가치가 기록이된다!!
+    # 중복 사용 가능 -> 앞에서부터 보자!
+    dp = [-inf for _ in range(m + 1)]
+    # 
+    dp[0] = 0
+    for i in range(1, m + 1):
+        for w, v in infos:
+            if w <= i:
+                dp[i] = max(dp[i], dp[i - w] + v)
+    return max(dp)
+
+print(sol_1())
