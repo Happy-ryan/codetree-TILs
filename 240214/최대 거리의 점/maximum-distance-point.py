b6336f45e88f8586b7c77b7746e7e6ff7dcb9eab
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-14 19:53:11 +0900

    Title: 최대 거리의 점, Time: 294ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240214/최대 거리의 점/maximum-distance-point.py
@@ -0,0 +1,36 @@
+n, m = map(int, input().split())
+points = [int(input()) for _ in range(n)]
+
+def get_count(dist):
+    # 가장 왼쪽은 반드시 설치
+    # 두 인접한 사이의 거리의 최솟값이 dist일 때 설치할 수 있는 곳의 수
+    # 처음 기준은 맨 왼쪽..
+    # 왼쪽1 왼쪽2 ....
+    # 왼쪽1과 왼쪽2의 차이가 최소거리보다 작아
+    # 왼쪽1 왼쪽2 왼쪽3 과 비교해여함.
+    # 기준은 왼쪽이 1이 된다.
+    # 착각: 무조건 서로 인접한 것들의 차이를 구하는 것은 아니다!
+    cnt = 1
+    standard = points[0]
+    for i in range(1, n):
+        if abs(points[i] - standard) >= dist:
+            cnt += 1
+            standard = points[i]
+
+    return cnt
+
+
+def binary_search(target):
+    l, r = 1, max(points)
+    ans = -1
+    while l <= r:
+        mid = (l + r) // 2
+        if get_count(mid) >= target:
+            ans = mid
+            l = mid + 1
+        else:
+            r = mid -1
+
+    return ans
+
+print(binary_search(m))
