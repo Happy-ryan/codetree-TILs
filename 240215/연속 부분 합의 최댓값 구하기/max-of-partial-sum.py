2b79422dc99ff88317acadfec09efbc7cd874665
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-15 22:29:57 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240215/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
+++ 240215/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -1,6 +1,7 @@
 n = int(input())
 nums = [0] + list(map(int, input().split()))
-dp = [-1 for _ in range(n + 1)]
+inf = int(1e9)
+dp = [-inf for _ in range(n + 1)]
 
 # dp[i]: 마지막으로 인덱스i를 '반드시' 선택했을 때, 만들 수 있는 부부분수열 합의 최대값
 # '연속' 부분 수열이으모 i 입장에서는 자기만 선택하거나 바로 앞 + 자기를 선택해야한다.
@@ -9,4 +10,4 @@ dp[0] = nums[0]
 for i in range(1, n + 1):
     dp[i] = max(nums[i], dp[i - 1] + nums[i])
 
-print(dp[n])
+print(max(dp))
