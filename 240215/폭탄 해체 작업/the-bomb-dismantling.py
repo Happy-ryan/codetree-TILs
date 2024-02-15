96cb1d24d5505d48a964037729859f2670385fe9
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-15 20:48:30 +0900

    Title: 폭탄 해체 작업, Time: 346ms, Memory: 28MB, Status: Passed - CodeTree

--- 240215/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240215/폭탄 해체 작업/the-bomb-dismantling.py
@@ -2,11 +2,25 @@
 # 8/1(8), 2/1(2), 10/3(3.xx), 7/5(1.xx)
 # 1초 박에 안걸림!! 1초 선택해서 해체해도 2초 것도 해체 가능!!
 # 같은 초에 있으면 점수 큰거..!
-MAX_TIME = 10001
-dp = [0] * MAX_TIME
-t = int(input())
-for _ in range(t):
-    score, time = map(int, input().split())
-    dp[time] = max(dp[time], score)
+from heapq import heappush, heappop
 
-print(sum(dp))
+t = int(input())
+bombs = [list(map(int, input().split())) for _ in range(t)]
+bombs.sort(key= lambda x: (x[1], x[0]))
+MAX_TIME = bombs[-1][-1]
+
+bomb_list = [[] for _ in range(MAX_TIME + 1)]
+for score, limit in bombs:
+    bomb_list[limit].append(score)
+
+heap = []
+cnt = 0
+for time in range(MAX_TIME, 0, -1):
+    for score in bomb_list[time]:
+        heappush(heap, -score)
+    if len(heap) == 0:
+        continue
+    q = -heappop(heap)
+    cnt += q
+    
+print(cnt)
