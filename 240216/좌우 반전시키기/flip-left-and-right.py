dc584c7daf5958277ffdd607bb4e92d96e38bfa4
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-16 22:23:45 +0900

    Title: 좌우 반전시키기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240216/좌우 반전시키기/flip-left-and-right.py
@@ -0,0 +1,19 @@
+# 순차적 진행(중복선택은 절대로 안돼) & 꼭 선택할 부분
+# 자기자신 + 양옆이 모두 반전!!
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+cnt = 0
+for i in range(1, n):
+    if nums[i - 1] == 0:
+        cnt += 1
+        nums[i - 1] = 1
+        nums[i] = 0 if nums[i] == 1 else 1
+        if i < n - 1:
+            nums[i + 1] =  0 if nums[i + 1] == 1 else 1
+
+if nums[n - 1] == 0:
+    print(-1)
+else:
+    print(cnt)
