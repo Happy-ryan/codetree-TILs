e31cf296206c4d57e982c261f13bd724d46c4f56
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-02-16 18:47:36 +0900

    Title: 회의실 겹치지 않게 하기, Time: 518ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240216/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -0,0 +1,15 @@
+n = int(input())
+cmds = [list(map(int, input().split())) for _ in range(n)]
+# 회의실
+# 끝나는 시간이 이를수록 더 많은 회의를 넣을 수 있다.
+# 취소를 최소...회의를 최대화!!
+cmds.sort(key=lambda x: (x[1], -x[1]))
+
+last = cmds[0][1]
+cnt = 1
+for start, end in cmds[1:]:
+    if last <= start:
+        cnt += 1
+        last = start
+
+print(n - cnt)
