c0dca88cef02b94980da82783faf5f4422f62b02
Happy-ryan <101412264+Happy-ryan@users.noreply.github.com>
2024-03-29 20:56:57 +0900

    Title: 대응되는 수와 문자, Time: 418ms, Memory: 40MB, Status: Passed - CodeTree

--- 240329/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240329/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -3,17 +3,15 @@ strings = [input() for _ in range(n)]
 qs = [input() for _ in range(m)]
 
 def solution(n, m, strings, qs):
-    dic1 = {}
-    dic2 = {}
+    dic = {}
     for idx, s in enumerate(strings):
-        dic1[s] = idx + 1
-        dic2[idx + 1] = s
+        dic[s] = idx + 1
 
     for q in qs:
         if q.isdigit():
-            print(dic2[int(q)])
+            print(strings[int(q) - 1])
         else:
-            print(dic1[q])
+            print(dic[q])
 
 
 solution(n, m, strings, qs)
